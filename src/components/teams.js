d2bb9c42d4f2dd1ccda31de068b176e8356537e2
CarlBalansag <carl@techccb.com>
2023-10-22 00:15:59 -0700

    Completed adding API to past season component
    Used api to get teams stats on each nba team now just needs to edit some of the css and design

--- src/components/teams.js
+++ src/components/teams.js
@@ -1,39 +1,5 @@
 import React, { Component } from "react";
 
-const url = 'https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '0aecbba164msh7f920f485034affp13bb34jsn2de9f4ab946f',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-	}
-};
-
-async function getTeams(){
-    try {
-        const response = await fetch(url, options)
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                data = data.response
-
-                for (let i = 0; i <= 30; i++) {
-                    console.log(data[i])
-                }
-            });
-
-
-    } catch (error) {
-        console.error(error);
-    }
-
-
-}
-
-getTeams()
-
-
 function Teams(props) {
 
 
@@ -41,14 +7,17 @@ function Teams(props) {
     return (
         <div className="flex flex-col max-w-lg min-w-[60%] ml-10 w-10/12  sm:w-10/12 h-64 bg-purple-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 border border-gray-100">
             <div id="logo" className="h-48">
-                <img className="w-40 mt-2 ml-32 sm:w-40 sm:mt-2 sm:mr-11" src="https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png" alt="NBA LOGO" />
+                <p className="text-white text-lg pl-[170px] mt-1">{props.name}</p>
+                <img className="w-36 mt-2 ml-32 sm:w-36 sm:mt-2 sm:mr-11" src={props.img} alt="NBA LOGO" />
             </div>
             <div id="info" className="h-20  grid grid-cols-2 gap-2">
                 <div className="">
-                    <p className="text-4xl text-emerald-500">72</p>
+                    <p className="pl-24 text-emerald-500">Wins</p>
+                    <p className="pl-24 text-3xl text-emerald-500">{props.win}</p>
                 </div>
                 <div className="">
-                    <p className="text-4xl text-rose-800">81</p>
+                    <p className="pl-16 text-rose-800">Loss</p>
+                    <p className="pl-16 text-3xl text-rose-800">{props.loss}</p>
                 </div>
             </div>
         </div>
--- src/pages/pastSeason.js
+++ src/pages/pastSeason.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/navbar';
 import Teams from '../components/teams';
-import data from '../data/pastSeason.json'
+
+
+const url = 'https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022';
+const options = {
+method: 'GET',
+headers: {
+    'X-RapidAPI-Key': '0aecbba164msh7f920f485034affp13bb34jsn2de9f4ab946f',
+    'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+}
+};
 
 function PastSeason() {
+
+    const [stats, setStats] = useState([]);
+
+    useEffect(() => {
+        
+
+        fetch(url, options)
+        .then(res => res.json())
+        .then(data => setStats(data.response))
+            .catch(err => console.log(err.message));
+        
+    }, [])
+
+
     return (
         <>
         <Navbar />
         <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 gap-y-10 lg:grid-cols-3 3xl:grid-cols-4'>
-            <Teams />
-            <Teams />
-            <Teams />
-            <Teams />
-            <Teams />
+            {stats?.map(item => {
+                return <Teams name={item.team.name} img={item.team.logo} win={item.win.total} loss={item.loss.total} />
+            })}
         </div>
         </>
     );
 }
 
 export default PastSeason;
+
+// name img win loss
